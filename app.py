bf7a6ef79fb36fe3c457ed87be671d10906ef34b
Brandon <b.cmoderndesigns@gmail.com>
2021-07-17 10:43:55 -0700

    changed menu storage into dict with ingredients, you can now save create menus and use them later

--- app.py
+++ app.py
@@ -2,27 +2,52 @@
 import random   
 from docx import Document
 from datetime import date
+import ast
 
-def import_dinners():
-    res = []
-    f = open("dinners.txt","r")
-    for line in f:
-        res.append(line)
+def import_dinner_dict():
+    file = open("dinners_dict.txt", "r")
+    contents = file.read()
+    res = ast.literal_eval(contents)
+    file.close()
     return res
-
-dinners = import_dinners()
-
+    
+dinners = import_dinner_dict()
 days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
 def create_menu():
-    menu = [random.choice(dinners) for x in range(6)]
-    return menu
+    menu = []
+    res = []
+    for key in dinners.keys():
+        menu.append(key)
+    for num in range(6):
+        res.append(random.choice(menu))
+    return res
 
 def add_meal():
     new_meal = input("what would you like to add?")
-    dinners.append(new_meal)
+    ingredients = input("what ingredients do you use?")
+    ingredient_array = ingredients.split(",")
+    print(ingredient_array)
+    dinners[new_meal] = ingredient_array
+    f = open("dinners_dict.txt", "w")
+    f.write(str(dinners)) 
+    f.close()
     
-def create_docx(menu):
+
+def create_docx():
+    new_or_saved = input('would you like to use a saved menu? (y/n) \n: ')
+    if "y" in new_or_saved.lower():
+        menus = read_saved_menus()
+        menu_index = []
+        i = 0
+        for key,val in menus.items():
+            print(i,key,val)
+            menu_index.append(key)
+            i += 1
+        pick_meal = input('Which meal would you like to use? pick corrisponding number (0-x) \n: ')
+        menu = menus[menu_index[int(pick_meal)]]
+    else:
+        menu = create_menu()
     doc = Document()
     p = doc.add_paragraph()
     for day in range(6):
@@ -32,5 +57,30 @@ def create_docx(menu):
     f.write(str(date.today()) + str(menu))
     f.close() 
     
-create_docx(create_menu())
 
+def generate_menu():
+    menu = create_menu()
+    [print(days[x]+ ': ' + menu[x]) for x in range(len(menu))]
+    save = input('does this menu look good to you? (y/n) to save.')
+    if save.lower() == "y":
+        f = open('saved_menus.txt','a')
+        f.write(str(menu) +'|' + str(date.today()))
+        f.close()
+    
+    
+def read_saved_menus():
+    f = open('saved_menus.txt','r')
+    saved_menus = {}
+    for line in f:
+        menu=[]
+        seperate = line.split('|')
+        m = seperate[0][1:-1]
+        mm = m.split(',')
+        for item in mm:
+            menu.append(item.strip("' "))
+        date = seperate[1]
+        saved_menus[date] = menu
+    return saved_menus
+    
+
+create_docx()
