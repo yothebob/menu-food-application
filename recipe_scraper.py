2d2d92b1671c8f9ba508dc50da6fcb439263df16
yothebob <b.cmoderndesigns@gmail.com>
2021-07-24 15:25:24 -0700

    finished scraper for based.cooking, saved all recipes to based_cooking_scrape.txt, needs cleaning

--- recipe_scraper.py
+++ recipe_scraper.py
@@ -1,4 +1,4 @@
-import re 
+import re
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,7 +7,7 @@ based_cooking = r.text
 soup = BeautifulSoup(based_cooking,'html.parser')
 
 recipes = {}
-
+links = []
 for recipe in soup.find_all("li"):
     #finding recipe names, then adding to dict
     cut_end = str(recipe)[0:-9]
@@ -20,9 +20,22 @@ for recipe in soup.find_all("li"):
     right_quotation = cut_left_html.rfind('"')
     recipe_html = cut_left_html[1:right_quotation]
     #go into recipe html to get ingredients
-    print(recipe_name,recipe_html)
-    recipe_r = requests.get('https://based.cooking/' + str(recipe.html))
+    links.append(str(recipe_html))
+
+for link in links:
+    recipe_r = requests.get('https://based.cooking/' + str(link))
     text = recipe_r.text
     recipe_soup = BeautifulSoup(text,'html.parser')
-    print("it worked!")
-    print(recipe_soup.prettify())
+    recipe_name = recipe_soup.find_all("h1")
+    recipe_ingredient_list = recipe_soup.find_all("ul")
+    recipe_ingredient_list = str(recipe_ingredient_list).split("</li>")
+    recipe_ingredients = []
+    for item in recipe_ingredient_list:
+        recipe_ingredients.append(item[5:])
+    recipe_name = str(recipe_name)[5:-6]
+    recipes[str(recipe_name)] = recipe_ingredients
+print(recipes)
+f = open("based_cooking_scrape.txt","w")
+f.write(str(recipes))
+f.close()
+print("FINISHED!!!!")
