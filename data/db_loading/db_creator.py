96f7d04bd67e3ad4ed3ad68ba4c826ee3ea2b70a
yothebob <b.cmoderndesigns@gmail.com>
2021-08-21 08:47:20 -0700

    working on creating db more

--- data/db_loading/db_creator.py
+++ data/db_loading/db_creator.py
@@ -6,7 +6,15 @@ con = sqlite3.connect('meals.db')
 cur = con.cursor()
 
 # get dictionary out of txt file
-dinners = open("dinner_dict.txt","r")
+dinners = open("dinners_dict.txt","r")
 content = dinners.read()
 dinner_dict = ast.literal_eval(content)
 dinners.close()
+
+
+#cur.execute("CREATE TABLE dinners (id INTEGER PRIMARY KEY, name TEXT UNIQUE, ingredients TEXT)")
+ i = 0
+for keys, values in dinner_dict.items():
+    print("key",keys,"Values",values)
+    i += 1
+    cur.execute("INSERT INTO dinners VALUES (?, ?, ?)",(i, keys, values))
