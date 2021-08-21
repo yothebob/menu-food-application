b9a846e5cb90a98b37305c4fd32fe03a78d44dad
yothebob <b.cmoderndesigns@gmail.com>
2021-08-20 20:01:21 -0700

    read dinners_dict.txt for sqlite3

--- data/db_loading/db_creator.py
+++ data/db_loading/db_creator.py
@@ -1,1 +1,12 @@
 import sqlite3
+import ast
+
+con = sqlite3.connect('meals.db')
+
+cur = con.cursor()
+
+# get dictionary out of txt file
+dinners = open("dinner_dict.txt","r")
+content = dinners.read()
+dinner_dict = ast.literal_eval(content)
+dinners.close()
